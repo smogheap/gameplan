d6b2b6de8f8429872ad1e2743a091c666ee1309b
Owen Swerkstrom <owenswerk@gmail.com>
2015-05-04 18:02:02 -0500

    slightly more twin-stick-y, not really :^)

---
+++ prototype/plowerdefense/asset/egg.js
@@ -0,0 +1,41 @@
+LOAD({
+	"name": "egg",
+	"scale": 0.34,
+	"above": [
+		{
+			"name": "cell",
+			"image": "image/eggcell.png",
+			"pivot": {
+				"x": 256,
+				"y": 256
+			},
+			"rotate": 0,
+			"scale": 0.5,
+			"alpha": 1,
+			"offset": {
+				"x": 0,
+				"y": 0
+			},
+			"above": [
+				{
+					"name": "nucleus",
+					"image": "image/eggcell.png",
+					"offset": {
+						"x": 256,
+						"y": 256
+					},
+					"rotate": 0,
+					"scale": 0.4,
+					"alpha": 1,
+					"offset": {
+						"x": 256,
+						"y": 256
+					}
+				}
+			]
+		}
+	],
+	"below": [
+	],
+	"pose": {}
+});
