b7a33598a4ee0b18765f2c504c912b06e453e4ec
Owen Swerkstrom <owenswerk@gmail.com>
2015-05-08 17:33:00 -0500

    basic wave support (win or -/+ to change level)

--- prototype/plowerdefense/src/plowerdefense.js
+++ prototype/plowerdefense/src/plowerdefense.js
@@ -6,7 +6,7 @@ var PD = {
 	scene: null,
 	json: {},
 	thing: {},
-	activesperm: 4,
+	text: {},
 	sperms: [],
 	shots: [],
 	input: {
@@ -17,8 +17,164 @@ var PD = {
 		menu: false
 	},
 	speed: 0,
-	oldangle: 0
+	oldangle: 0,
+	level: null,
+	startwave: 0,
+	activesperm: 0,
+	remaining: 0,
+	spermspeed: 0,
+	spermvary: 0,
+	spermmeander: 0,
+	spermorbit: 0,
+	spermchase: 0,
+	victory: 0,
 };
+var LEVELS = [
+	{
+		title: "Lock and load!",
+		total: 10,
+		active: 3,
+		speed: 0.5,
+		speedvary: 0.2,
+		meander: 1,
+		orbit: 0,
+		chase: 0
+	}, {
+		title: "Come at me!",
+		total: 10,
+		active: 3,
+		speed: 0.5,
+		speedvary: 0.2,
+		meander: 0,
+		orbit: 0,
+		chase: 1
+	}, {
+		title: "Time to get spanked!",
+		total: 10,
+		active: 5,
+		speed: 1,
+		speedvary: 0.5,
+		meander: 0,
+		orbit: 1,
+		chase: 0
+	}, {
+		title: "You wanna piece?",
+		total: 20,
+		active: 1,
+		speed: 3,
+		speedvary: 0.5,
+		meander: 0,
+		orbit: 0,
+		chase: 1
+	}, {
+		title: "You Suck!",
+		total: 30,
+		active: 10,
+		speed: 2,
+		speedvary: 0.5,
+		meander: 0.2,
+		orbit: 0.6,
+		chase: 0.2
+	}, {
+		title: "Yo, hit me, bro!",
+		total: 30
+	}, {
+		title: "LOL OMG NO HOMO!",
+		total: 30
+	}, {
+		title: "Eat your protein!",
+		total: 30
+	}, {
+		title: "Ladies first, asshole!",
+		total: 30
+	}, {
+		title: "What up, bay-bee?",
+		total: 30
+	}, {
+		title: "OMG that was the worst!",
+		total: 50
+	}, {
+		title: "Tomorrow = Beer shits",
+		total: 50
+	}, {
+		title: "Plan B",
+		total: 50
+	}, {
+		title: "Thanks, guys!",
+		total: 50
+	}, {
+		title: "Get off me, bro!",
+		total: 50
+	}, {
+		title: "Your chocolate / my peanut butter!",
+		total: 50
+	}, {
+		title: "Where's the beef??",
+		total: 50
+	}, {
+		title: "You mean that's it?!",
+		total: 50
+	}, {
+		title: "Oh...my...God!",
+		total: 50
+	}, {
+		title: "Next!!!!",
+		total: 50
+	}, {
+		title: "What's your name again?",
+		total: 50
+	}, {
+		title: "Like a glove!",
+		total: 50
+	}, {
+		title: "What...there's more???",
+		total: 50
+	}, {
+		title: "We've all had worse.",
+		total: 50
+	}, {
+		title: "You smell that?",
+		total: 50
+	}, {
+		title: "Do that again!",
+		total: 50
+	}, {
+		title: "That thing you like.",
+		total: 50
+	}, {
+		title: "Now roll over.",
+		total: 50
+	}, {
+		title: "Uh! I love you.",
+		total: 50
+	}, {
+		title: "Thank you sir, may I have another?",
+		total: 50
+	}, {
+		title: "Sorry, you startled me!",
+		total: 50
+	}, {
+		title: "In the WHAT!?!?!?!",
+		total: 50
+	}, {
+		title: "k thx cya bye",
+		total: 50
+	}, {
+		title: "Aaaaaannnnd, dismount!",
+		total: 50
+	}, {
+		title: "No, YOU taste it!",
+		total: 50
+	}, {
+		title: "Whaddya mean, it slipped off??",
+		total: 50
+	}, {
+		title: "Wrong hole, doofus!",
+		total: 1000,
+		active: 100,
+		speed: 2
+	}
+];
 function LOAD(json) {
 	var data = json;
 	var name = data.name || "anonymous";
@@ -91,11 +247,63 @@ function shoot() {
 	});
 }
 
+function loadlevel(lvl, time) {
+	if(isNaN(lvl) || LEVELS.length < lvl) {
+		console.log("invalid level '" + lvl + "', using 0");
+		lvl = 0;
+	} else if(lvl < 0) {
+		console.log("invalid level'" + lvl + "', using final");
+		lvl = LEVELS.length - 1;
+	}
+	PD.level = lvl;
+	PD.text.lvl.setString("WAVE " + (lvl + 1));
+	PD.text.level.setString(LEVELS[lvl].title);
+	PD.text.lvl.setVisible(true);
+	PD.text.level.setVisible(true);
+	PD.sperms.splice(0, PD.sperms.length);
+	PD.activesperm = 0;
+	PD.remaining = LEVELS[lvl].total || 10;
+
+	PD.spermspeed = LEVELS[lvl].speed || 1;
+	PD.spermvary = LEVELS[lvl].speedvary || 0.5;
+	PD.spermmeander = isNaN(LEVELS[lvl].meander) ? 0.4 : LEVELS[lvl].meander;
+	PD.spermorbit = isNaN(LEVELS[lvl].orbit) ? 0.3 : LEVELS[lvl].orbit;
+	PD.spermchase = isNaN(LEVELS[lvl].chase) ? 0.3 : LEVELS[lvl].chase;
+
+	PD.startwave = time + 4000;
+}
+
 function tick(scene, time) {
 	var delshot = -1;
 	var delsperm = -1;
 	var dx = 0;
 	var dy = 0;
+
+	if(PD.level === null) {
+		loadlevel(0, time);
+	}
+
+	// victory flash and next level
+	if(PD.victory) {
+		if(Math.floor(PD.victory / 10) % 2) {
+			PD.scene.setBG("#fff");
+		} else {
+			PD.scene.setBG("#211");
+		}
+		PD.victory--;
+		if(!PD.victory) {
+			PD.scene.setBG("#211");
+			loadlevel(PD.level + 1, time);
+		}
+	}
+
+	// start wave if it's time
+	if(PD.startwave && time > PD.startwave) {
+		PD.startwave = 0;
+		PD.activesperm = LEVELS[PD.level].active || 4;
+		PD.text.lvl.setVisible(false);
+		PD.text.level.setVisible(false);
+	}
 
 	PD.thing.egg.$.cell.scale = 0.5 + (Math.sin(time / 200) * 0.02);
 	PD.thing.egg.$.nucleus.scale = 0.4 + (Math.cos(time / 200) * 0.05);
@@ -170,7 +378,6 @@ function tick(scene, time) {
 
 	// move sperms
 	PD.sperms.every(function(sperm) {
-
 		if(sperm.mode === 0) {
 			// idiot, swims erratically
 			sperm.rotate += (Math.random() - 0.5) * 10;
@@ -178,7 +385,7 @@ function tick(scene, time) {
 			// orbit
 			dx = sperm.x - PD.thing.egg.x;
 			dy = sperm.y - PD.thing.egg.y;
-			sperm.rotate = Math.atan2(dx, dy) / (Math.PI / -180);
+			sperm.rotate = (Math.atan2(dx, dy) / -TO_RADIANS) - 45;
 		} else if(sperm.mode === 2) {
 			// perfect chase
 			dx = PD.thing.egg.x - sperm.x;
@@ -199,9 +406,11 @@ function tick(scene, time) {
 	}
 
 	// send in more sperms
-	if(PD.sperms.length < PD.activesperm) {
+	if(PD.sperms.length < PD.activesperm && PD.remaining) {
+		PD.remaining--;
 		var x = Math.random() * PD.WIDTH;
 		var y = Math.random() * PD.HEIGHT;
+		var mode = 0;
 		if(Math.random() < 0.5) {
 			if(Math.random() < 0.5) {
 				x = 0;
@@ -215,20 +424,33 @@ function tick(scene, time) {
 				y = PD.HEIGHT;
 			}
 		}
+
+		mode = Math.random();
+		if(mode > PD.spermmeander + PD.spermorbit) {
+			mode = 2;
+		} else if(mode > PD.spermmeander) {
+			mode = 1;
+		} else {
+			mode = 0;
+		}
 		PD.sperms.push({
 			x: x,
 			y: y,
 			rotate: Math.random() * 360,
-			speed: Math.random() + 0.5,
-			mode: Math.floor(Math.random() * 3)
+			speed: (PD.spermspeed - PD.spermvary) +
+					(Math.random() * PD.spermvary * 2),
+			mode: mode
 		});
+	}
+	if(!PD.victory && !PD.remaining && !PD.sperms.length) {
+		PD.victory = 59;
 	}
 }
 
 function start() {
 	PD.scene = new penduinSCENE(PD.canvas, PD.WIDTH, PD.HEIGHT, tick);
 	PD.scene.setBG("#211");
-//	PD.scene.showFPS(true);
+	PD.scene.showFPS(true);
 	PD.scene.setAutoOrder(false);
 
 	PD.scene.addOBJ(PD.thing.egg);
@@ -236,12 +458,20 @@ function start() {
 	PD.thing.egg.y = PD.HEIGHT / 2;
 	PD.scene.addOBJ(PD.thing.sperm);
 	PD.thing.sperm.setInstances(PD.sperms);
-	PD.thing.shot.setInstances(PD.shots);
 
+	PD.thing.shot.setInstances(PD.shots);
 	PD.scene.addOBJ(PD.thing.shot);
 
 	PD.scene.addOBJ(PD.thing.aim);
-	PD.canvas.fireEvent
+
+	PD.text.lvl = new penduinTEXT("wave x", 30, "white", 0.5, 1, true);
+	PD.text.level = new penduinTEXT("title", 40, "white", 0.5, 0, true);
+	PD.text.lvl.setVisible(false);
+	PD.text.level.setVisible(false);
+	PD.text.lvl.x = PD.text.level.x = PD.WIDTH / 2;
+	PD.text.lvl.y = PD.text.level.y = PD.HEIGHT / 2;
+	PD.scene.addTEXT(PD.text.lvl);
+	PD.scene.addTEXT(PD.text.level);
 }
 
 window.addEventListener("load", function() {
@@ -303,6 +533,19 @@ function handlekey(event, down) {
 		break;
 	case 27:  //esc
 		PD.input.menu = down;
+		break;
+	case 61:  //+
+	case 107: //num+
+		if(down) {
+			PD.victory = 1;
+		}
+		break;
+	case 109: //num-
+	case 173: //-
+		if(down) {
+			PD.level -= 2;
+			PD.victory = 1;
+		}
 		break;
 	default:
 		return;
